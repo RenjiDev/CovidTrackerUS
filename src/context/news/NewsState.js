eb828ab11e6c78959910e02687bacbbc146302c4
JosephCon1998 <josephcon1998@gmail.com>
2020-05-03 02:08:03 -0400

    world news restyled

--- src/context/news/NewsState.js
+++ src/context/news/NewsState.js
@@ -8,65 +8,66 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 const NewsState = (props) => {
-  const initialState = {
-    loading: true,
-    news: [],
-    error: null,
-  };
+	const initialState = {
+		loading: true,
+		news: [],
+		error: null,
+	};
 
-  const [state, dispatch] = useReducer(NewsReducer, initialState);
+	const [state, dispatch] = useReducer(NewsReducer, initialState);
 
-  // Get news articles
-  const getNews = async () => {
-    let payload = [];
-    try {
-      const articles = await axios.get(
-        'https://cors-anywhere-covidtrackerus.herokuapp.com/https://www.google.com/search?q=covid+news&source=lnms&tbm=nws&sa=X&ved=2ahUKEwi00dyDiIPpAhXZXc0KHUXlAlQQ_AUoAXoECAwQAw&biw=1920&bih=898'
-      );
-      const $ = await cheerio.load(articles.data);
-      $('.nChh6e').each((i, el) => {
-        const artObj = {};
-        artObj.id = i;
-        const link = $(el).find('a').attr('href');
-        artObj.link = link;
-        const headline = $(el).find('.phYMDf').text();
-        artObj.headline = headline;
-        const subTitle = $(el).find('.eYN3rb').text();
-        artObj.subTitle = subTitle;
-        const provider = $(el).find('.pDavDe').text();
-        artObj.provider = provider;
-        const providerLogo = $(el).find('.wQYexc').find('img').attr('src');
-        artObj.providerLogo = providerLogo;
-        const img = $(el).find('.KNcnob').find('g-img').find('img').attr('src');
-        artObj.img = img;
-        const date = $(el).find('.eNg7of').text();
-        artObj.date = date;
-        payload.push(artObj);
-      });
-      payload = payload.filter((p) => p.headline);
-      dispatch({
-        type: GET_NEWS,
-        payload,
-      });
-    } catch (err) {
-      dispatch({
-        type: NEWS_ERROR,
-        payload: err,
-      });
-    }
-  };
+	// Get news articles
+	const getNews = async () => {
+		let payload = [];
+		try {
+			const articles = await axios.get(
+				'https://cors-anywhere-covidtrackerus.herokuapp.com/https://www.google.com/search?q=covid+news&source=lnms&tbm=nws&sa=X&ved=2ahUKEwi00dyDiIPpAhXZXc0KHUXlAlQQ_AUoAXoECAwQAw&biw=1920&bih=898'
+			);
+			const $ = await cheerio.load(articles.data);
+			$('.nChh6e').each((i, el) => {
+				const artObj = {};
+				artObj.id = i;
+				const link = $(el).find('a').attr('href');
+				artObj.link = link;
+				const headline = $(el).find('.phYMDf').text();
+				artObj.headline = headline;
+				console.log(headline);
+				const subTitle = $(el).find('.eYN3rb').text();
+				artObj.subTitle = subTitle;
+				const provider = $(el).find('.pDavDe').text();
+				artObj.provider = provider;
+				const providerLogo = $(el).find('.wQYexc').find('img').attr('src');
+				artObj.providerLogo = providerLogo;
+				const img = $(el).find('.KNcnob').find('g-img').find('img').attr('src');
+				artObj.img = img;
+				const date = $(el).find('.eNg7of').text();
+				artObj.date = date;
+				payload.push(artObj);
+			});
+			payload = payload.filter((p) => p.headline);
+			dispatch({
+				type: GET_NEWS,
+				payload,
+			});
+		} catch (err) {
+			dispatch({
+				type: NEWS_ERROR,
+				payload: err,
+			});
+		}
+	};
 
-  return (
-    <NewsContext.Provider
-      value={{
-        news: state.news,
-        loading: state.loading,
-        getNews,
-      }}
-    >
-      {props.children}
-    </NewsContext.Provider>
-  );
+	return (
+		<NewsContext.Provider
+			value={{
+				news: state.news,
+				loading: state.loading,
+				getNews,
+			}}
+		>
+			{props.children}
+		</NewsContext.Provider>
+	);
 };
 
 export default NewsState;
--- src/context/news/newsReducer.js
+++ src/context/news/newsReducer.js
@@ -1,19 +1,20 @@
 import { GET_NEWS, NEWS_ERROR } from '../Types';
 
 export default (state, action) => {
-  switch (action.type) {
-    case GET_NEWS:
-      return {
-        ...state,
-        news: action.payload,
-        loading: false,
-      };
-    case NEWS_ERROR:
-      return {
-        ...state,
-        error: action.payload,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case GET_NEWS:
+			console.log(action.payload);
+			return {
+				...state,
+				news: action.payload,
+				loading: false,
+			};
+		case NEWS_ERROR:
+			return {
+				...state,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
 };
