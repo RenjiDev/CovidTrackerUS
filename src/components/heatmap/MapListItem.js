90d08129819ff6ae54e538f9fd3d0393e451a727
JosephCon1998 <josephcon1998@gmail.com>
2020-05-04 02:04:34 -0400

    heatmap almost there!

--- src/components/heatmap/MapListItem.js
+++ src/components/heatmap/MapListItem.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import HeatmapContext from '../../context/heatmap/heatmapContext';
+import jump from 'jump.js';
 
-const MapListItem = ({ name, total }) => {
-  return (
-    <div className="heatmap-list-item">
-      <span>
-        <h1>{name}</h1>
-        <h2>{total}</h2>
-      </span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="#656565"
-      >
-        <path d="M13.25 7c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25zm10.75 5c0 6.627-5.373 12-12 12s-12-5.373-12-12 5.373-12 12-12 12 5.373 12 12zm-2 0c0-5.514-4.486-10-10-10s-10 4.486-10 10 4.486 10 10 10 10-4.486 10-10zm-13-2v2h2v6h2v-8h-4z" />
-      </svg>
-    </div>
-  );
+const MapListItem = ({ name, total, setShowList }) => {
+	const heatmapContext = useContext(HeatmapContext);
+	const { setCurrentState } = heatmapContext;
+
+	const showMoreStateInfo = () => {
+		setCurrentState(name);
+		setShowList(true);
+		jump('.heatmap-states-list-container', { duration: 500 });
+	};
+
+	return (
+		<div className='heatmap-list-item'>
+			<span>
+				<h1>{name}</h1>
+				<h2>{total} cases</h2>
+			</span>
+			<svg
+				onClick={() => showMoreStateInfo()}
+				xmlns='http://www.w3.org/2000/svg'
+				width='24'
+				height='24'
+				viewBox='0 0 24 24'
+				fill='#656565'
+			>
+				<path d='M13.25 7c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25zm10.75 5c0 6.627-5.373 12-12 12s-12-5.373-12-12 5.373-12 12-12 12 5.373 12 12zm-2 0c0-5.514-4.486-10-10-10s-10 4.486-10 10 4.486 10 10 10 10-4.486 10-10zm-13-2v2h2v6h2v-8h-4z' />
+			</svg>
+		</div>
+	);
 };
 
 export default MapListItem;
--- src/components/heatmap/USMap.js
+++ src/components/heatmap/USMap.js
@@ -28,9 +28,9 @@ const offsets = {
 	DC: [49, 21],
 };
 
-const USMap = () => {
+const USMap = ({ setShowList }) => {
 	const heatMapContext = useContext(HeatmapContext);
-	const { getStateData } = heatMapContext;
+	const { getStateData, setCurrentState } = heatMapContext;
 
 	useEffect(() => {
 		getStateData();
@@ -40,6 +40,167 @@ const USMap = () => {
 	const gradeBColor = '#A20028'; // #C70039
 	const gradeCColor = '#86001C'; // #A20028
 	const gradeDColor = '#6A0010'; // #710011
+
+	const handleClick = (stateID) => {
+		let state = '';
+		switch (stateID) {
+			case 'AK':
+				state = 'Alaska';
+				break;
+			case 'AL':
+				state = 'Alabama';
+				break;
+			case 'AR':
+				state = 'Arkansas';
+				break;
+			case 'AZ':
+				state = 'Arizona';
+				break;
+			case 'CA':
+				state = 'California';
+				break;
+			case 'CO':
+				state = 'Colorado';
+				break;
+			case 'CT':
+				state = 'Connecticut';
+				break;
+			case 'DC':
+				state = 'District of Columbia';
+				break;
+			case 'DE':
+				state = 'Delaware';
+				break;
+			case 'FL':
+				state = 'Florida';
+				break;
+			case 'GA':
+				state = 'Georgia';
+				break;
+			case 'HI':
+				state = 'Hawaii';
+				break;
+			case 'IA':
+				state = 'Iowa';
+				break;
+			case 'ID':
+				state = 'Idaho';
+				break;
+			case 'IL':
+				state = 'Illinois';
+				break;
+			case 'IN':
+				state = 'Indiana';
+				break;
+			case 'KS':
+				state = 'Kansas';
+				break;
+			case 'KY':
+				state = 'Kentucky';
+				break;
+			case 'LA':
+				state = 'Louisiana';
+				break;
+			case 'MA':
+				state = 'Massachusetts';
+				break;
+			case 'MD':
+				state = 'Maryland';
+				break;
+			case 'ME':
+				state = 'Maine';
+				break;
+			case 'MI':
+				state = 'Michigan';
+				break;
+			case 'MN':
+				state = 'Minnesota';
+				break;
+			case 'MO':
+				state = 'Missouri';
+				break;
+			case 'MS':
+				state = 'Mississippi';
+				break;
+			case 'MT':
+				state = 'Montana';
+				break;
+			case 'NC':
+				state = 'North Carolina';
+				break;
+			case 'ND':
+				state = 'North Dakota';
+				break;
+			case 'NE':
+				state = 'Nebraska';
+				break;
+			case 'NH':
+				state = 'New Hampshire';
+				break;
+			case 'NJ':
+				state = 'New Jersey';
+				break;
+			case 'NM':
+				state = 'New Mexico';
+				break;
+			case 'NV':
+				state = 'Nevada';
+				break;
+			case 'NY':
+				state = 'New York';
+				break;
+			case 'OH':
+				state = 'Ohio';
+				break;
+			case 'OK':
+				state = 'Oklahoma';
+				break;
+			case 'OR':
+				state = 'Oregon';
+				break;
+			case 'PA':
+				state = 'Pennsylvania';
+				break;
+			case 'RI':
+				state = 'Rhode Island';
+				break;
+			case 'SC':
+				state = 'South Carolina';
+				break;
+			case 'SD':
+				state = 'South Dakota';
+				break;
+			case 'TN':
+				state = 'Tennessee';
+				break;
+			case 'TX':
+				state = 'Texas';
+				break;
+			case 'UT':
+				state = 'Utah';
+				break;
+			case 'VA':
+				state = 'Virginia';
+				break;
+			case 'VT':
+				state = 'Vermont';
+				break;
+			case 'WA':
+				state = 'Washington';
+				break;
+			case 'WI':
+				state = 'Wisconsin';
+				break;
+			case 'WV':
+				state = 'West Virginia';
+				break;
+			case 'WY':
+				state = 'Wyoming';
+				break;
+		}
+		setCurrentState(state);
+		setShowList(true);
+	};
 
 	function getStateColor(cur) {
 		let returnColor = '';
@@ -82,6 +243,7 @@ const USMap = () => {
 											geography={geo}
 											fill={getStateColor(cur)}
 											className='geo-state'
+											onClick={() => handleClick(cur.id)}
 										/>
 									);
 								})}
