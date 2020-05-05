7ec3015993c064dc986fb08503b219085769f9d0
JosephCon1998 <josephcon1998@gmail.com>
2020-05-05 01:13:03 -0400

    TABLE HAS FILTER :)

--- src/components/all-data/Table.js
+++ src/components/all-data/Table.js
@@ -1,117 +1,91 @@
-import React, { useContext, useEffect } from 'react';
-import { useTable } from 'react-table';
-
+import React, { useContext, useState, useEffect } from 'react';
 import CountryContext from '../../context/country/countryContext';
 
 const Table = () => {
-  const countryContext = useContext(CountryContext);
-  const { countries, filtered } = countryContext;
-  const tableData = [];
-  countries.forEach((country) => {
-    if (
-      country.country !== 'North-America' &&
-      country.country !== 'South-America' &&
-      country.country !== 'Europe' &&
-      country.country !== 'Asia' &&
-      country.country !== 'Africa' &&
-      country.country !== 'Diamond-Princess-' &&
-      country.country !== 'MS-Zaandam-'
-    ) {
-      tableData.push({
-        col1: country.country,
-        col2: country.cases.total.toLocaleString(),
-        col3: country.cases.new
-          ? `+${parseInt(country.cases.new).toLocaleString()}`
-          : '',
-        col4: country.deaths.new
-          ? `+${parseInt(country.deaths.new).toLocaleString()}`
-          : '',
-        col5: country.cases.active.toLocaleString(),
-        col6: country.cases.recovered.toLocaleString(),
-        col7: country.deaths.total.toLocaleString(),
-      });
-    }
-  });
+	const countryContext = useContext(CountryContext);
+	const { countries, filtered } = countryContext;
+	const [tableData, setTableData] = useState([]);
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Country',
-        accessor: 'col1', // accessor is the "key" in the data
-        width: 500,
-      },
-      {
-        Header: 'Confirmed',
-        accessor: 'col2',
-        width: 500,
-      },
-      {
-        Header: 'New Cases',
-        accessor: 'col3',
-        width: 500,
-      },
-      {
-        Header: 'New Deaths',
-        accessor: 'col4',
-        width: 500,
-      },
-      {
-        Header: 'Active',
-        accessor: 'col5',
-        width: 500,
-      },
-      {
-        Header: 'Recovered',
-        accessor: 'col6',
-        width: 500,
-      },
-      {
-        Header: 'Deaths',
-        accessor: 'col7',
-        width: 500,
-      },
-    ],
-    []
-  );
-  const data = React.useMemo(() => [...tableData], []);
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data });
-
-  return (
-    <div className="table-container">
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className='table-container'>
+			<table>
+				<thead>
+					<th>Country</th>
+					<th>Confirmed</th>
+					<th>New Cases</th>
+					<th>New Deaths</th>
+					<th>Active</th>
+					<th>Recovered</th>
+					<th>Deaths</th>
+				</thead>
+				<tbody>
+					{!filtered
+						? countries.map((value) => {
+								if (
+									value.country !== 'North America' &&
+									value.country !== 'South America' &&
+									value.country !== 'Europe' &&
+									value.country !== 'Asia' &&
+									value.country !== 'Africa' &&
+									value.country !== 'Diamond-Princess-' &&
+									value.country !== 'MS-Zaandam-'
+								) {
+									return (
+										<tr>
+											<td>{value.country}</td>
+											<td>{value.cases.total.toLocaleString()}</td>
+											<td>
+												{value.cases.new
+													? `+${parseInt(value.cases.new).toLocaleString()}`
+													: ''}
+											</td>
+											<td>
+												{value.deaths.new
+													? `+${parseInt(value.deaths.new).toLocaleString()}`
+													: ''}
+											</td>
+											<td>{value.cases.active.toLocaleString()}</td>
+											<td>{value.cases.recovered.toLocaleString()}</td>
+											<td>{value.cases.total.toLocaleString()}</td>
+										</tr>
+									);
+								}
+						  })
+						: filtered.map((value) => {
+								if (
+									value.country !== 'North America' &&
+									value.country !== 'South America' &&
+									value.country !== 'Europe' &&
+									value.country !== 'Asia' &&
+									value.country !== 'Africa' &&
+									value.country !== 'Diamond-Princess-' &&
+									value.country !== 'MS-Zaandam-'
+								) {
+									return (
+										<tr>
+											<td>{value.country}</td>
+											<td>{value.cases.total.toLocaleString()}</td>
+											<td>
+												{value.cases.new
+													? `+${parseInt(value.cases.new).toLocaleString()}`
+													: ''}
+											</td>
+											<td>
+												{value.deaths.new
+													? `+${parseInt(value.deaths.new).toLocaleString()}`
+													: ''}
+											</td>
+											<td>{value.cases.active.toLocaleString()}</td>
+											<td>{value.cases.recovered.toLocaleString()}</td>
+											<td>{value.cases.total.toLocaleString()}</td>
+										</tr>
+									);
+								}
+						  })}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default Table;
--- src/context/heatmap/heatmapReducer.js
+++ src/context/heatmap/heatmapReducer.js
@@ -1,62 +1,63 @@
 import {
-  GET_STATE_DATA,
-  STATE_ERROR,
-  SET_CURRENT_STATE,
-  CLEAR_CURRENT_STATE,
-  FILTER_STATE,
-  CLEAR_FILTER,
+	GET_STATE_DATA,
+	STATE_ERROR,
+	SET_CURRENT_STATE,
+	CLEAR_CURRENT_STATE,
+	FILTER_STATE,
+	CLEAR_FILTER,
 } from '../Types';
 
 export default (state, action) => {
-  switch (action.type) {
-    case GET_STATE_DATA:
-      return {
-        ...state,
-        states: action.payload,
-        loading: false,
-      };
-    case STATE_ERROR: //For debugging purposes
-      return {
-        ...state,
-        error: action.payload,
-      };
-    case SET_CURRENT_STATE:
-      return {
-        ...state,
-        currentState: action.payload.state,
-        grade: action.payload.grade,
-        positive: action.payload.positive,
-        recovered: action.payload.recovered,
-        hospitalized: action.payload.hospitalizedCurrently,
-        deaths: action.payload.death,
-        lastUpdated: action.payload.dateModified,
-      };
-    case CLEAR_CURRENT_STATE:
-      return {
-        ...state,
-        states: null,
-        currentState: null,
-        grade: null,
-        positive: null,
-        recovered: null,
-        hospitalized: null,
-        deaths: null,
-        lastUpdated: null,
-      };
-    case FILTER_STATE:
-      return {
-        ...state,
-        filtered: state.states.filter((s) => {
-          const regex = new RegExp(`^${action.payload}`, 'i');
-          return s.state.match(regex);
-        }),
-      };
-    case CLEAR_FILTER:
-      return {
-        ...state,
-        filtered: null,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case GET_STATE_DATA:
+			return {
+				...state,
+				states: action.payload,
+				loading: false,
+			};
+		case STATE_ERROR: //For debugging purposes
+			return {
+				...state,
+				error: action.payload,
+			};
+		case SET_CURRENT_STATE:
+			return {
+				...state,
+				currentState: action.payload.state,
+				grade: action.payload.grade,
+				positive: action.payload.positive,
+				recovered: action.payload.recovered,
+				hospitalized: action.payload.hospitalizedCurrently,
+				deaths: action.payload.death,
+				lastUpdated: action.payload.dateModified,
+			};
+		case CLEAR_CURRENT_STATE:
+			return {
+				...state,
+				states: null,
+				currentState: null,
+				grade: null,
+				positive: null,
+				recovered: null,
+				hospitalized: null,
+				deaths: null,
+				lastUpdated: null,
+			};
+		case FILTER_STATE:
+			return {
+				// Fix this shit :)
+				...state,
+				filtered: state.states.filter((s) => {
+					const regex = new RegExp(`^${action.payload}`, 'i');
+					return s.state.match(regex);
+				}),
+			};
+		case CLEAR_FILTER:
+			return {
+				...state,
+				filtered: null,
+			};
+		default:
+			return state;
+	}
 };
