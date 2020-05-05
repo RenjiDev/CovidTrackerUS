46828da76a8605e29db87ac8dd6ef2204f5dca97
Hassan Badat <hassanbadat@cpe-174-104-134-156.neo.res.rr.com>
2020-05-05 03:05:42 -0400

    the charts are wack

--- src/App.js
+++ src/App.js
@@ -17,52 +17,52 @@ import AllData from './pages/all-data/AllData';
 import Sources from './pages/sources/Sources';
 
 const indexContent = {
-	title: 'COVID-19 Tracker US',
-	description: 'Quickly track cases of COVID-19 around the world.',
-	url: 'https://covidtrackerus.org/',
-	image: 'https://covidtrackerus.org/icon512.png',
+  title: 'COVID-19 Tracker US',
+  description: 'Quickly track cases of COVID-19 around the world.',
+  url: 'https://covidtrackerus.org/',
+  image: 'https://covidtrackerus.org/icon512.png',
 };
 
 ReactGA.initialize('UA-163780283-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
-	return (
-		<CountryState>
-			<Helmet
-				title={indexContent.title}
-				meta={[
-					{
-						name: 'description',
-						property: 'og:description',
-						content: indexContent.description,
-					},
-					{ property: 'og:title', content: indexContent.title },
-					{ property: 'og:url', content: indexContent.url },
-					{ property: 'og:image', content: indexContent.image },
-					{ property: 'og:image:type', content: 'image/png' },
-					{ property: 'og:twitter:image:src', content: indexContent.image },
-					{ property: 'og:twitter:title', content: indexContent.title },
-					{
-						property: 'og:twitter:description',
-						content: indexContent.description,
-					},
-				]}
-			/>
-			<NewsState>
-				<HeatmapState>
-					<Fragment>
-						<Landing />
-						<Summary />
-						<Heatmap />
-						<AllData />
-						<News />
-						<Sources />
-					</Fragment>
-				</HeatmapState>
-			</NewsState>
-		</CountryState>
-	);
+  return (
+    <CountryState>
+      <Helmet
+        title={indexContent.title}
+        meta={[
+          {
+            name: 'description',
+            property: 'og:description',
+            content: indexContent.description,
+          },
+          { property: 'og:title', content: indexContent.title },
+          { property: 'og:url', content: indexContent.url },
+          { property: 'og:image', content: indexContent.image },
+          { property: 'og:image:type', content: 'image/png' },
+          { property: 'og:twitter:image:src', content: indexContent.image },
+          { property: 'og:twitter:title', content: indexContent.title },
+          {
+            property: 'og:twitter:description',
+            content: indexContent.description,
+          },
+        ]}
+      />
+      <NewsState>
+        <HeatmapState>
+          <Fragment>
+            <Landing />
+            <Summary />
+            <Heatmap />
+            <AllData />
+            <News />
+            <Sources />
+          </Fragment>
+        </HeatmapState>
+      </NewsState>
+    </CountryState>
+  );
 };
 
 export default App;
